81b62369336b1b184d3484c30cce5ebcdb3af69f
Kelvy Tagli <kelvytagliacolli@hotmail.com.br>
2023-12-31 22:48:41 -0300

    Feat: links and new section

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,13 +1,23 @@
-import { Container } from "./styles"
+import { Container, Links } from "./styles"
 
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 
 export function Details() {
 
     return (
         <Container>
             <Header/>
+            <Section title="links úteis">
+                <Links>
+                    <li>
+                        <a href="http:\\github.com/kelvytagli" target="_blank">github</a>
+                    </li>
+                    <li>items</li>
+                    <li>items</li>
+                </Links>
+            </Section>
             <Button title='Entrar' loading/>
         </Container>
     )
