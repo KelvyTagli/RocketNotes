f3b55a0728a3b7ca8aeca4b2511240ac206e398b
Kelvy Tagli <kelvytagliacolli@hotmail.com.br>
2024-01-04 20:29:47 -0300

    Feat: page signIn :white_check_mark: and new page SignUp

--- src/Pages/SignUp/index.jsx
+++ src/Pages/SignUp/index.jsx
@@ -4,26 +4,27 @@ import { Input } from '../../components/Input'
 
 import { Button } from '../../components/Button'
 
-import { EnvelopeSimple, LockSimple } from "@phosphor-icons/react";
+import { EnvelopeSimple, LockSimple, User } from "@phosphor-icons/react";
 
 
-export function SignIn() {
+export function SignUp() {
     return(
         <Container>
+            <Background/>
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links úteis</p>
                 
-                <h2>Faça seu login</h2>
+                <h2>Criar sua conta</h2>
+                <Input placeholder="Nome" type="text" icon={User}/>
                 <Input placeholder="E-mail" type="text" icon={EnvelopeSimple}/>
                 <Input placeholder="Senha" type="password" icon={LockSimple}/>
 
-                <Button title='Entrar'/>
+                <Button title='Cadastrar'/>
                 <a href="#">
-                    Criar conta
+                    Voltar para o login
                 </a>
             </Form>
-            <Background/>
         </Container>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignIn />
+      <SignIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
